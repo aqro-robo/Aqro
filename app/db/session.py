b7bc73a0a4e14272e402f2b34953786fe0482ab7
aqro-io <aqro.io@gmail.com>
2025-05-16 14:32:10 +0000

    initial commit

---
+++ app/db/session.py
@@ -0,0 +1,5 @@
+ from sqlalchemy import create_engine from sqlalchemy.orm import sessionmaker, declarative_base 
+import os DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/aqro") 
+engine = create_engine(DATABASE_URL) SessionLocal = sessionmaker(autocommit=False, 
+autoflush=False, bind=engine)
+Base = declarative_base()
